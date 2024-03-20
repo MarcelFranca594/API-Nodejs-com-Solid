6b4b9b068fd1909e50bc314f1051334fd8c3dabd
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2024-03-20 09:13:50 -0400

    Integracao com Prisma ORM - Adiciona código Node.js/TypeScript com Prisma para criar e conectar-se a um banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+// Criando conexão com o banco de dados utilizando o Prisma
+const prisma = new PrismaClient()
+
+// Criando um novo usuário no banco de dados
+prisma.user.create({
+  data: {
+    name: 'Marcel Igor',
+    email: 'marcel@rocketseat.com.br'
+  }
+})
